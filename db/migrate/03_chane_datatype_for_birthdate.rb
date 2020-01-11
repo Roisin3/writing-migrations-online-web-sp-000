6fbe2946f2d79c21074cd93d7387668508fa197f
Michael Lynch <michael@michaelrlynch.com>
2020-01-11 02:37:37 +0000

    Automatically backed up by Learn

--- db/migrate/03_chane_datatype_for_birthdate.rb
+++ db/migrate/03_chane_datatype_for_birthdate.rb
@@ -1,4 +1,4 @@
-class ChangeDatatypeForBirthdate
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
   def ChangeDatatypeForBirthdate
     change_column :students, :birthdate, :datetime
   end
